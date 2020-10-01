11b820f3472bc49e9ad6b73869205ed9067e75da
Sunil Kumar <sunil.kumar@girnarsoft.com>
2020-10-01 13:18:37 +0530

    component session

--- app.js
+++ app.js
@@ -1,3 +1,16 @@
+Vue.component('greeting' , {
+	template:'<p>This is an component template, I am {{name}} <button v-on:click="changeName">Change name</button></p>',
+	data:function(){
+		return {
+			name: "Sunil"
+		}
+	},
+	methods: {
+		changeName: function() {
+			this.name = "Sushil"
+		}
+	}
+})
 var instaceOne = new Vue({
 	el: '#vue-app-one',
 	data: {
