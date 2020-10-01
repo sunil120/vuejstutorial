585a8040fff2a0267a780575adede7e07cae15c4
Sunil Kumar <sunil.kumar@girnarsoft.com>
2020-10-01 12:55:21 +0530

    multiple instace

--- app.js
+++ app.js
@@ -1,46 +1,31 @@
-new Vue({
-	el: '#vue-app',
+var instaceOne = new Vue({
+	el: '#vue-app-one',
 	data: {
-		name: "Sunil Kumar",
-		age : 0,
-		website : 'http://www.google.com',
-		websiteTag : '<a href="http://www.google.com">Website Tag</a>',
-		x:0,
-		y:0,
-		error: false,
-		success:false,
-		todods: ['Node','Vue','Express','Angular'],
-		users: [
-			{ name:'Sunil' , age:30 },
-			{ name:'Sushil' , age:30 }
-		],
-		health:100,
-		ended:false
+		title: "App one"
 	},
 	methods: {
-		courses: function(course) {
 
-			return "Lets Start Learning " + course + " " + this.name;
-		},
-		subtractAge:  function(year) {
-			this.age = this.age-year;
-		},
-		addAge:  function(year) {
-			this.age = this.age+year;
-		},
-		updateXY: function(event) {
-			this.x = event.offsetX
-			this.y = event.offsetY
-		},
-		punch: function() {
-			this.health -=10;
-			if(this.health <=0) {
-				this.ended= true;
-			}
-		},
-		restart: function() {
-			this.health=100;
-			this.ended=false;
+	},
+	computed: {
+		greet: function() {
+			return "This block belongs app one"
+		}
+	}
+});
+
+var instaceTwo = new Vue({
+	el: '#vue-app-two',
+	data: {
+		title: "App Two"
+	},
+	methods: {
+		changeAppOneTitle: function() {
+			instaceOne.title = "Changed from the App two"
+		}
+	},
+	computed: {
+		greet: function() {
+			return "This block belongs app one"
 		}
 	}
 });
